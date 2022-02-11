ab5f1f6a660933fd88b4a6f98d505b52d9dcde9f
Richard Aiello <richaiello17@gmail.com>
2022-02-11 10:42:37 -0500

    created starting varibles
    pulled necessary elements from DOM, created API variable for convenient fetching, decalred an undefined endpoint.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-// BaseURL* https://firestore.googleapis.com/v1/projects/calorie-tracking-app-d89d9/databases/(default)/documents/${endpoint}
-// Create a FetchWrapper class
-// Define const API utilizing FetchWrapper and BaseURL
-// Store necessary elements from the DOM in variables
 // Implement a function to display food items in the pantry
 // Create eventListeners for both forms working with fetch and the REST API
+
+// BaseURL* https://firestore.googleapis.com/v1/projects/calorie-tracking-app-d89d9/databases/(default)/documents/${endpoint}
 import FetchWrapper from "./fetch-wrapper";
+
+const API = new FetchWrapper(`https://firestore.googleapis.com/v1/projects/calorie-tracking-app-d89d9/databases/(default)/documents/`);
+
+const pantryForm = document.querySelector('.access-pantry');
+const pantryId = document.querySelector('#access-pantry__name');
+const foodForm = document.querySelector('.food__form');
+const name = document.querySelector('#food__name');
+const carbs = document.querySelector('#carbs');
+const protein = document.querySelector('#protein');
+const fat = document.querySelector('#fat');
+const pantryName = document.querySelector('.pantry__heading');
+const list = document.querySelector('.pantry__list');
+const calories = document.querySelector('.calories__total');
+
+let endpoint;
