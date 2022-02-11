bb218e468aa06d9fad87d091a5ca2d178b49708b
Richard Aiello <richaiello17@gmail.com>
2022-02-11 10:04:05 -0500

    Pseudo code for first steps in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // BaseURL* https://firestore.googleapis.com/v1/projects/calorie-tracking-app-d89d9/databases/(default)/documents/${endpoint}
+// Create a FetchWrapper class
+// Define const API utilizing FetchWrapper and BaseURL
+// Store necessary elements from the DOM in variables
+// Implement a function to display food items in the pantry
+// Create eventListeners for both forms working with fetch and the REST API
